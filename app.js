6a9f6e919caf757d038329f01f2fc6b5b67f03b1
Marina Bearman <marinabearman@gmail.com>
2018-05-25 15:31:53 -0700

    Fix port for remote

--- app.js
+++ app.js
@@ -84,10 +84,10 @@ app.use('/admin/posts', posts);
 app.use('/admin/categories', categories);
 app.use('/admin/comments', comments);
 
-
-
-
-
-app.listen(4500, ()=>{
-    console.log(`listening on port 4500`);
-});
+const port = process.env.PORT || 4500;
+app.listen(port, ()=>{
+        console.log(`listening on port ${port}`);
+    });
+// app.listen(4500, ()=>{
+//     console.log(`listening on port 4500`);
+// });
