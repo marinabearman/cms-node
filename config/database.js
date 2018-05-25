fe57e9ce87e808371b8342324f725973d02ca9c2
Marina Bearman <marinabearman@gmail.com>
2018-05-25 16:02:44 -0700

    Dynamic database env var

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-module.exports = {
-    //mongoDbUrl: 'mongodb://localhost:27017/cms'
-    //password for mlab is g0atsbe@rd but nned to change @ to %40
-    mongoDbUrl: 'mongodb://cms-user:g0atsbe%40rd@ds135760.mlab.com:35760/cms'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod-database');
+} else {
+    module.exports = require('./dev-database');
 }
